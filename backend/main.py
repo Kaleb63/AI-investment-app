31733bbe5191287d0e0f5addd712168c8edb0f34
Kaleb <kla891mgs@gmail.com>
2026-09-06 09:31:43 -0400

    Add stock lookup endpoint

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,18 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Investment app backend is running"}
+
+stocks = {
+    "AAPL": {"price": 200, "shares": 5},
+    "NVDA": {"price": 180, "shares": 2},
+    "TSLA": {"price": 700, "shares": 3}
+}
+
+@app.get("/stock/{ticker}")
+def get_stock(ticker: str):
+    return {
+        "ticker": ticker,
+        "price": stocks[ticker]["price"],
+        "shares": stocks[ticker]["shares"]
+    }
+get_stock("NVDA")
